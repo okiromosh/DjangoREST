69cfd7d4f26e03cc7077ffffe6c300692a0ff687
okiromosh <moses.okiror93@gmail.com>
2024-02-19 15:34:30 +0300

    create serializer.py

--- base/models.py
+++ base/models.py
@@ -6,3 +6,6 @@ class Article(models.Model):
     author = models.CharField(max_length=200)
     email = models.EmailField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
