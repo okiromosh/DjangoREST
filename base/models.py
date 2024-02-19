deedb41496bb59658754f06d567251556adaf397
okiromosh <moses.okiror93@gmail.com>
2024-02-19 15:01:07 +0300

    create project and app

---
+++ base/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=400)
+    author = models.CharField(max_length=200)
+    email = models.EmailField()
+    date = models.DateTimeField(auto_now_add=True)
